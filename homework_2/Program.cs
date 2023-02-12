ebf0f1df79d105389f0699af510f91a19e206055
Maksim Bondarenko <m.bondarenko1988@yandex.ru>
2023-02-12 15:08:01 +0300

    Done second homework to nine seminar about C sharp

---
+++ homework_2/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int InputNumber(string str)
+{
+    int number;
+    string? text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Вы ввели не число");
+    }
+    return number;
+}
+
+int Akerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akerman(m - 1, 1);
+    }
+    else
+    //if (m>0 && n>0)
+    {
+        return Akerman(m - 1, Akerman(m, n - 1));
+    }
+}
+
+int m = InputNumber("Введите число M: ");
+int n = InputNumber("Введите число N: ");
+System.Console.WriteLine(Akerman(m, n));
